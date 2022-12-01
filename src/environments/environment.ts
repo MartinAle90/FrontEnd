da8b18ef3f9fec7a6d59e636042ab197fcaf67a7
MartinAle90 <99972612+MartinAle90@users.noreply.github.com>
2022-11-30 23:28:43 -0300

    cambio de Heroku a Koyeb

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,12 @@
 
 export const environment = {
   firebase: {
-    projectId: 'frontendmad90',
+    projectId: 'portfolioapmad90',
     appId: '1:1022706183237:web:0a0c6bc49d5b931a9c472c',
-    storageBucket: 'frontendmad90.appspot.com',
+    storageBucket: 'portfolioapmad90.appspot.com',
     locationId: 'southamerica-east1',
     apiKey: 'AIzaSyCDGun5jTL-UvIPrmytA50Mhzet8bCq4qw',
-    authDomain: 'frontendmad90.firebaseapp.com',
+    authDomain: 'portfolioapmad90.firebaseapp.com',
     messagingSenderId: '1022706183237',
   },
   production: false,
